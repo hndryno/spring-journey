7d768354f0d183cb894c4fb8250825494cc5f6c3
hendriyono <hendriyono97@gmail.com>
2022-04-21 20:05:44 +0700

    learn singleton

---
+++ src/main/java/henresearch/spring/core/Database.java
@@ -0,0 +1,17 @@
+package henresearch.spring.core;
+
+public class Database {
+
+    private static Database database;
+
+    public static Database getInstance(){
+        if(database == null){
+            database = new Database();
+        }
+        return database;
+    }
+
+    private Database() {
+
+    }
+}
---
+++ src/test/java/henresearch/spring/core/DatabaseTest.java
@@ -0,0 +1,14 @@
+package henresearch.spring.core;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DatabaseTest {
+    @Test
+    void testSingleton(){
+        var database1 = Database.getInstance();
+        var database2 = Database.getInstance();
+
+        Assertions.assertSame(database1, database2);
+    }
+}
