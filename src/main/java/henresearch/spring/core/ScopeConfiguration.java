909998f0ccb0ed7e3fcc4d682fb5cba9a2e3cde1
hendriyono <hendriyono97@gmail.com>
2022-04-30 18:29:39 +0700

    learn kind of scope in springboot

---
+++ src/main/java/henresearch/spring/core/ScopeConfiguration.java
@@ -0,0 +1,33 @@
+package henresearch.spring.core;
+
+import henresearch.spring.core.data.Foo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Slf4j
+@Configuration
+public class ScopeConfiguration {
+
+    //pengertian scope
+    //scope adalah strategy cara sebuah object dibuat
+    //secara default strategy object ddi spring adalah singleton
+    //akses pertama kedua dan ketiga adalah object yang sama, jika singleton
+
+    //macam macam scope
+    //1. singleton = default hanya dibuat sekali dalam spring ioc
+    //2. prototipe = selalu dibuat object baru setiap mengakses bean
+    //3. request = dibuat baru per http request (hanya untuk webApp)
+    //4. session = dibuat per http session (hanya untuk web app)
+    //5. application = dibuat baru per servletContext (hanya uintuk web App)
+    //6 websocket = dibuat baru per webSocket (hanya untuk web socket app)
+
+    @Bean
+    @Scope("prototype")  //contoh scope
+    public Foo foo(){
+        log.info("create bean");
+        return new Foo();
+    }
+
+}
---
+++ src/test/java/henresearch/spring/core/ScopeTest.java
@@ -0,0 +1,35 @@
+package henresearch.spring.core;
+
+import henresearch.spring.core.data.Foo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ScopeTest {
+    private ApplicationContext applicationContext;
+
+    @BeforeEach
+    void setUp() {
+        applicationContext = new AnnotationConfigApplicationContext(ScopeConfiguration.class);
+    }
+
+    @Test
+    void testPrototipeScope(){
+
+        Foo foo1 = applicationContext.getBean(Foo.class);
+
+        Foo foo2 = applicationContext.getBean(Foo.class);
+
+        Foo foo3 = applicationContext.getBean(Foo.class);
+
+        Assertions.assertNotSame(foo1, foo2);
+
+        Assertions.assertNotSame(foo1, foo3);
+
+        Assertions.assertNotSame(foo2, foo3);
+
+
+    }
+}
